98b94ce9d7f5a98eaf173c60933859a547d1a0a9
ElonB <elobr959@student.liu.se>
2019-04-01 13:45:50 +0200

    Lab 1 code done

--- lab1/part3.R
+++ lab1/part3.R
@@ -2,16 +2,16 @@
 mu = 2.39
 obsRad = c(-2.44, 2.14, 2.54, 1.83, 2.02, 2.33, -2.79, 2.23, 2.07, 2.02)
 
-kGrid <- seq(0.01, 20, by=0.01)
+kGrid <- seq(0.01, 10, by=0.001)
 
-priorDist <- rexp(kGrid, 1)
+priorDist <- dexp(kGrid)
 plot(priorDist)
 kappa = kGrid
 
 dvonmises <- function(y, mu, kappa){
   res = 1
   for (i in y){
-      res = res*(exp(kappa*cos(i-mu))/(2*pi*besselI(kappa,1)))
+      res = res*(exp(kappa*cos(i-mu))/(2*pi*besselI(kappa,0)))
   }
   return(res)
 }
@@ -22,5 +22,8 @@ posterior = likeliHood*priorDist
 
 posterior = (posterior/sum(posterior))/0.01
 
-plot(posterior)
-likeliHood
+plot(kGrid, posterior)
+
+md = Mode(posterior)
+index = which(posterior == max(posterior))
+k = kGrid[2116]
