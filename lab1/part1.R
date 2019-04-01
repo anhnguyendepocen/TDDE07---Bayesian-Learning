6e49b4fd3cf2e148f408d8746d313b80ae6ee45b
Ludwig Thaung <ludth852@student.liu.se>
2019-04-01 13:24:46 +0200

    updated part 1 and 2

--- lab1/part1.R
+++ lab1/part1.R
@@ -15,24 +15,29 @@ print(paste0("ground truth Mean: ", p))
 
 BetaRandomPosteriorPriorPlotandMeanStdComparison <- function(a,b,n,p){
   xGrid <- seq(0.001, 0.999, by=0.001)
-  normalizedLikelihood = dbeta(xGrid, n*p+1, n*(1-p)+1)
+  normalizedLikelihood = dbeta(xGrid, 20*p+1, 20*(1-p)+1)
   prior = dbeta(xGrid, a, b)
-  posterior = dbeta(xGrid, a+n*p, b+n*(1-p))
-  posteriorAlpha = a+n*p
-  posteriorBeta = b+n*(1-p)
+  posterior = dbeta(xGrid, a+20*p, b+20*(1-p))
+  posteriorAlpha = a+20*p
+  posteriorBeta = b+20*(1-p)
   posteriorRandom <- rbeta(n, posteriorAlpha, posteriorBeta)
   maxDensity <- max(normalizedLikelihood, prior, posterior) # Use to make the y-axis high enough
   
-  plot(xGrid, normalizedLikelihood, type = 'l', lwd = 3, col = "blue", xlim <- c(0,1), ylim <- c(0, maxDensity), xlab = "theta", 
-       ylab = 'Density', main = 'Bernoulli model - Beta(a,b) prior')
-  lines(xGrid, posterior, lwd = 3, col = "red")
-  lines(xGrid, prior, lwd = 3, col = "green")
-  legend(x = 0.01, y = maxDensity*0.95, legend = c("Likelihood (normalized)", "Prior", "Posterior"), col = c("blue","green","red"), lwd = c(3,3,3), cex = 0.7)
+  #plot(xGrid, normalizedLikelihood, type = 'l', lwd = 3, col = "blue", xlim <- c(0,1), ylim <- c(0, maxDensity), xlab = "theta", 
+  #     ylab = 'Density', main = 'Bernoulli model - Beta(a,b) prior')
+  #lines(xGrid, posterior, lwd = 3, col = "red")
+  #lines(xGrid, prior, lwd = 3, col = "green")
+  #legend(x = 0.01, y = maxDensity*0.95, legend = c("Likelihood (normalized)", "Prior", "Posterior"), col = c("blue","green","red"), lwd = c(3,3,3), cex = 0.7)
   
+  posteriorMean = posteriorAlpha/(posteriorAlpha + posteriorBeta)
+  print(paste0('Posterior mean GT: ', posteriorMean))
   print(paste0("ground truth std: ", sqrt(posteriorAlpha*posteriorBeta/(((posteriorAlpha+posteriorBeta)^2)*(posteriorAlpha+posteriorBeta+1)))))
   print(paste0("std: ", sd(posteriorRandom)))
   print(paste0("Mean: ", mean(posteriorRandom)))
   
+  #b part
+  posteriorAlpha = a+20*p
+  posteriorBeta = b+20*(1-p)
   posteriorRandomDraw <- rbeta(10000, posteriorAlpha, posteriorBeta)
   posteriorRandomOverCondition = which(posteriorRandomDraw < 0.4)
   probabilityCondition = length(posteriorRandomOverCondition)/length(posteriorRandomDraw)
@@ -41,14 +46,17 @@ BetaRandomPosteriorPriorPlotandMeanStdComparison <- function(a,b,n,p){
   print(paste0("propability condition with random: ", probabilityCondition))
   print(paste0("ground truth probability: ", trueProbability))
   
+  #c part
   logOdds = log(posteriorRandomDraw/(1 - posteriorRandomDraw))
   density(logOdds)
   # Kommentera bort hist om du vill köra det bort kommenterade ovan, om båda delarna plottas så kommer manipulate-panelen inte upp.
-  #hist(logOdds)
+  hist(logOdds)
+  #hist(posteriorRandomDraw)
+  #hist(posteriorRandomDraw/(1 - posteriorRandomDraw))
 }
 
 
 manipulate(
   BetaRandomPosteriorPriorPlotandMeanStdComparison(alpha,beta,num,p),
-  num = slider(1, 1000, step=1, initial = 2, label = "Number of trials")
+  num = slider(1, 10000, step=1, initial = 2, label = "Number of trials")
 )
--- lab1/part2.R
+++ lab1/part2.R
@@ -6,26 +6,11 @@ logY = log(y)
 meanY = sum(y)/length(y)
 meanLogY = sum(logY)/length(logY)
 
-getCdfValue <- function(y, target){
-  sortedY = sort(y)
-  n = length(sortedY)
-  i = 0
-  for(dataPoint in sortedY){
-    i = i + 1
-    if(dataPoint >= target){
-      return(i/n)
-    }
-  }
-}
-
 getDistrubutionFromPoints <- function(x){
   n = length(x)
   sortedX = sort(x)
   cumPercent = (1:n)/n
   densityPercent = (cumPercent[2:n] - cumPercent[1:n-1])/(sortedX[2:n] - sortedX[1:(n-1)])
-  #dataMatrix = matrix(1:(2*n-2), nrow=n-1, ncol=2, byrow=TRUE)
-  #dataMatrix[ ,1] = sortedX[2:n]
-  #dataMatrix[ ,2] = densityPercent
   
   dataMatrix = matrix(1:(2*n), nrow=n, ncol=2, byrow=TRUE)
   dataMatrix[ ,1] = sortedX
@@ -71,10 +56,11 @@ drawFromPosterior <- function(mean, n, logy){
   
   #plotting
   attach(mtcars)
-  par(mfrow=c(2,2))
+  par(mfrow=c(4,2))
   plot(xGrid, posterior, type = 'l', lwd = 3, col = "blue", xlim <- c(0,1), ylim <- c(0, maxDensity), xlab = "theta", 
        ylab = 'Density', main = 'Chi Posterior of Variance')
   plot(sort(posteriorRandomDraw))
+  hist(posteriorRandomDraw)
   
   #part b)
   giniTheory = 2*pnorm(posteriorRandomDraw/sqrt(2), mean = 0, sd = 1) - 1
@@ -83,7 +69,9 @@ drawFromPosterior <- function(mean, n, logy){
   distrubtionGiniLog = getDistrubutionFromPoints(logGini)
   distrubtionGini = getDistrubutionFromPoints(giniTheory)
   plot(distrubtionGiniLog)
+  hist(logGini)
   plot(distrubtionGini)
+  hist(giniTheory)
   
   #part c)
   lowerData = getIntervalData(giniTheory, 0, 0.025)
