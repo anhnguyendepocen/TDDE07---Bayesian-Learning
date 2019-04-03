72abbb3f7f2f607b8a03c24c758f47686d1cb818
ludth852 <ludth852@su15-123.ad.liu.se>
2019-04-03 14:04:36 +0200

    Changed the scaling in plots, need to make better axis-names in plots

--- lab1/part3.R
+++ lab1/part3.R
@@ -2,28 +2,29 @@
 mu = 2.39
 obsRad = c(-2.44, 2.14, 2.54, 1.83, 2.02, 2.33, -2.79, 2.23, 2.07, 2.02)
 
-kGrid <- seq(0.01, 10, by=0.001)
+kGrid <- seq(0.001, 10, by=0.001)
 
 priorDist <- dexp(kGrid)
-plot(priorDist)
+plot(kGrid, priorDist, )
 kappa = kGrid
 
 dvonmises <- function(y, mu, kappa){
   res = 1
   for (i in y){
-      res = res*(exp(kappa*cos(i-mu))/(2*pi*besselI(kappa,0)))
+    res = res*(exp(kappa*cos(i-mu))/(2*pi*besselI(kappa,0)))
   }
   return(res)
 }
 
 likeliHood <- dvonmises(obsRad, mu, kappa)
-plot(likeliHood)
+plot(kGrid, likeliHood/(sum(likeliHood)*0.001))
 posterior = likeliHood*priorDist
 
-posterior = (posterior/sum(posterior))/0.01
+posterior = (posterior/sum(posterior))/0.001
 
 plot(kGrid, posterior)
 
 md = Mode(posterior)
 index = which(posterior == max(posterior))
-k = kGrid[2116]
+k = kGrid[index]
+print(k)
