d19b86e89ab8a9d293acd0d9d8d45476f7c03a8c
Ludwig Thaung <ludwig.thaung@gmail.com>
2019-05-02 17:33:35 +0200

    finished 1a

--- lab2/part1.R
+++ lab2/part1.R
@@ -10,19 +10,22 @@ x = TempLinkoping['time']
 y = TempLinkoping['temp']
 x['time2'] = x^2
 x['1'] = x['time']/x['time']
+ph = x['time']
+#Just to have the betas in right order
+x['time'] = x['1']
+x['1'] = x['time2']
+x['time2'] = ph
 
 matrix_x = data.matrix(x)
 matrix_y = data.matrix(y)
 
 mu0 = c(-10,100,-100)
 omega0 = matrix(0.01, 3, 3)
-onesM = one(3, 3)
 v0 = 4
 sigmasq0 = 1
 
 e = rnorm(1, mean = 0, sd = sigmasq0)
 
-plot(TempLinkoping)
 
 betahat = inv((t(matrix_x)%*%matrix_x))%*%(t(matrix_x)%*%matrix_y) 
 
@@ -31,7 +34,20 @@ omegaN = t(matrix_x)%*%matrix_x + omega0
 vN = v0 + 3
 vNsigmaN2 = v0*sigmasq0 + (t(matrix_y)%*%matrix_y + t(mu0)%*%omega0%*%mu0 - t(muN)%*%omegaN%*%muN)
 
-randomSigma2 <- 
-randomBeta <- rnorm(n=10,mean = muN, sigmas)
+randomSigma2 <- rinvchisq(n = 10, df = vN, scale = (vNsigmaN2/vN))
+randomBetas <- c()
+plot(TempLinkoping, col="black") 
+for(singleSigma in randomSigma2) {
+  randomBeta <- rmvt(n = 10,mu = muN, S = singleSigma*inv(omegaN))
 
-
+  for(k in 1:10) {
+    ys = c()
+    xs = c()
+    for (i in 1:1000) {
+      y = randomBeta[k, 1] + randomBeta[k, 2]*i/1000 + randomBeta[k, 3]*(i/1000)^2
+      ys = c(ys, y)
+      xs = c(xs, i/1000)
+    }
+    lines(xs, ys, col="red") 
+  }
+}
